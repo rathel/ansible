268f5913d52f3c67342792bde714596e7dcea587
rathel <rathel@gmail.com>
2019-05-17 06:04:02 -0600

    Long list of updates

---
+++ k3ssetup.sh
@@ -0,0 +1,1 @@
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.11.201:6443 K3S_TOKEN=K1007aa6842059affb45b7d5e05caed4c52181ce9fe87f1c0459f065f886adcb690::node:29d296afaabce480897cab235be09390 sh -
